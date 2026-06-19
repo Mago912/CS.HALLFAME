70343c924f09614cc25fadc163546eea7c27fcb3
Mago912 <juangonzaleezz91@gmail.com>
2026-06-19 01:06:01 -0300

    Background color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,8 @@
     <link rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
-<body>
-    background-color: #010409;
-}
+<body style="background-color: #010409;">
+
 <nav class="navbar navbar-expand-lg" style="background-color: #1b1f24;">
     <div class="container">
         <a class="navbar-brand text-light fw-bold" href="/">
